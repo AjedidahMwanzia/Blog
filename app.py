ba093d3619e6599fe9281d6259bbc8e3d7b4233d
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-14 23:36:36 +0300

    add error page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template
+from . import views
 
-app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return  "hello dear"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template ('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return  render_template ('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
